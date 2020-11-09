09db8f9fe086566d4531fab13e0b0172d34514bc
Ian Glen Neal <ian.gl.neal@gmail.com>
2020-11-09 15:46:58 -0500

    Generate reg assignments

--- ada_hack/generate.py
+++ ada_hack/generate.py
@@ -4,17 +4,65 @@ import json
 import jinja2
 from jinja2 import Template
 
+width = {
+    "int": 32,
+}
+size = {
+    "int": 1,
+}
+index_width = {
+    "int": 1,
+}
+
+def generate_const(reg, prog, statement):
+    reg_type = prog['locals'][reg]
+    w = width[reg_type]
+    value = statement['value']['value']
+    lines = []
+    port = f"{w}'d{value}"
+
+    return port, lines
+
+def assign_reg(gname, prog, statement):
+    reg = statement['name']
+    value = statement['value']
+    lines = []
+
+    lines += [ f"{gname}[done] = {reg}.done;" ]
+    lines += [ f"{reg}.write_en = 1'd1;"]
+
+    port = None
+    if value['type'] == 'const':
+        port, sublines = generate_const(reg, prog, statement)
+
+    lines += sublines
+    lines += [ f"{reg}.in = {port};"]
+
+    return {'name': gname, 'lines': lines}
+
+def gen_group(gname, prog, statement):
+    typ = statement['type']
+    if typ == 'assignment':
+        name = statement['name']
+        if name in prog['locals']:
+            # We know it's a register
+            return assign_reg(gname, prog, statement)
+    return None
+
+def generate_groups(prog):
+    groups = []
+    for group_num, statement in enumerate(prog['statements']):
+        name = f'group_{group_num}'
+        group = gen_group(name, prog, statement)
+        if group is not None:
+            groups += [group]
+
+    return groups
+
 def generate(prog): 
     tmpl_file = sys.argv[1]
-    width = {
-        "int": 32,
-    }
-    size = {
-        "int": 1,
-    }
-    index_width = {
-        "int": 1,
-    }
+
+    groups = generate_groups(prog)
 
     futil = None
     with open(tmpl_file) as f:
@@ -23,7 +71,8 @@ def generate(prog):
             prog=prog,
             width=width,
             size=size,
-            index_width=index_width
+            index_width=index_width,
+            groups=groups
         )
 
     outfile = sys.argv[3]
